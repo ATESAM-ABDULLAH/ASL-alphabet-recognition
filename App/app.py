f6ab5a398b757b9bf553a350744394946175f08c
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-05-03 23:02:19 +0500

    Combined all functions in camera.py

--- App/app.py
+++ App/app.py
@@ -1,9 +1,15 @@
 #Import necessary libraries
 from flask import Flask, render_template, Response
-import cv2
+from camera import *
 
 #Initialize the Flask app
 app = Flask(__name__)
+
+#Default app route
+@app.route('/')
+def video_feed():
+   return 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
