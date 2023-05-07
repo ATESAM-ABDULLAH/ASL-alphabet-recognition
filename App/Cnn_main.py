2f68dc58e3c826a8e492950b87a9b298048a6463
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-05-07 22:06:58 +0500

    Code completed

--- App/Cnn_main.py
+++ App/Cnn_main.py
@@ -32,21 +32,32 @@ def camera(top_left:list=[0,40],bottom_right:list=[300,340])->'Webcam':
     vid = cv2.VideoCapture(0)# define a video capture object
 
     while(True): #Loop over frames/images till closed
-        connected, frame = vid.read()# Capture the video frame by frame
-        if(not connected):#Connection not succesful
+        # Capture the video frame by frame
+        connected, frame = vid.read()
+        #Connection not succesful
+        if(not connected):
             break
 
         # Make a detection area -> model input
         cropframe=frame[top_left[1]:bottom_right[1],top_left[0]:bottom_right[0]]
-        # Make a rectangle on frame
-        frame=cv2.rectangle(frame,top_left,bottom_right,255,2) 
+        
         # Predict img
-        char,accuracy = predict_img(cropframe)      # Predict img
-        # A solid background for text
+        char,accuracy = predict_img(cropframe)     
+        
+        # Draw Detection Area on frame
+        frame=cv2.rectangle(frame,top_left,bottom_right,255,2) 
+
+        # A solid background for Model output
         cv2.rectangle(frame, (0,0), (300, 40), (245, 117, 16), -1)
-        # Show Model output
+        
+        # Show Model output text
         cv2.putText(frame,f"Char: {char} - {round(accuracy,2)}% ", 
                     (3,30), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+        # Show message for exit
+        cv2.putText(frame,"Press 'q' to quit", 
+                    (170,460), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
         # Display the resulting frame
         cv2.imshow('Webcam', frame)        
         #Press 'q' to quit
